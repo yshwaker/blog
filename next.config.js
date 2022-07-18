e52fddafa04f1d48457be0e8311a393c1858cce0
yshwaker <yshwaker@gmail.com>
2022-07-18 17:42:06 +0800

    setup splitbee

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/bee.js',
+        destination: 'https://cdn.splitbee.io/sb.js',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import splitbee from '@splitbee/web'
 import Link from 'next/link'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: '/bee.js',
+    })
+  })
   return (
     <div className="mx-auto max-w-2xl py-8">
       <h1 className="text-lg mb-10">
