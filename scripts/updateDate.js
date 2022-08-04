381a24d6b58a0f6171afbd617172f2af04b42624
yshwaker <yshwaker@gmail.com>
2022-08-04 17:47:38 +0800

    update post date on commit

---
+++ scripts/updateDate.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises
+const matter = require('gray-matter')
+
+const updateFrontmatter = async () => {
+  const [, , ...mdFilePaths] = process.argv
+
+  mdFilePaths.forEach(async (path) => {
+    const file = matter.read(path)
+    const { data: currentFrontmatter } = file
+
+    if (currentFrontmatter.draft !== true) {
+      const updatedFrontmatter = {
+        ...currentFrontmatter,
+        updatedOn: new Date().toISOString(),
+      }
+      file.data = updatedFrontmatter
+      const updatedFileContent = matter.stringify(file)
+      fs.writeFile(path, updatedFileContent)
+    }
+  })
+}
+
+updateFrontmatter()
