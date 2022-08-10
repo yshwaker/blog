2a177abd2d0edcd719281b974bd981b317639c03
yshwaker <yshwaker@gmail.com>
2022-08-10 16:45:55 +0800

    fix sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -8,7 +8,7 @@ export const getServerSideProps = ({ res }) => {
     production: 'https://shioyu.dev',
   }[process.env.NODE_ENV]
 
-  const paths = getPostList().map(({ slug }) => `${baseUrl}/${slug}`)
+  const paths = getPostList().map(({ slug }) => `${baseUrl}/blog/${slug}`)
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${paths
