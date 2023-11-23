8c548b218d2019e9c957157f330284bb40ec9cdf
yshwaker <yshwaker@gmail.com>
2023-11-23 16:18:25 +0800

    feat: add note section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import dayjs from 'dayjs'
 import { type GetStaticProps, type InferGetStaticPropsType } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import heptabaseLinks from '../static/heptabase-links.json'
 
 export default function Home({
   posts,
@@ -16,7 +17,7 @@ export default function Home({
       </Head>
       <main>
         <h2 className="text-5xl text-shadow shadow-gray-400 font-bold text-gray-800 py-16 ">
-          Latest
+          Post
         </h2>
         <div className="space-y-12">
           {posts.map(({ title, date, summary, slug }) => (
@@ -30,6 +31,23 @@ export default function Home({
                 {dayjs(date).format('YYYY年MM月DD日')}
               </p>
               <p className="text-gray-500">{summary}</p>
+            </Link>
+          ))}
+        </div>
+        <h2 className="text-5xl text-shadow shadow-gray-400 font-bold text-gray-800 py-16 ">
+          Note
+        </h2>
+        <div className="space-y-12">
+          {heptabaseLinks.map(({ title, date, url }) => (
+            <Link key={url} href={url} className="group block">
+              <h3>
+                <span className="text-2xl font-bold text-gray-600 bg-clip-text transition duration-1000 group-hover:text-opacity-0 group-hover:duration-100 bg-gradient-to-r from-indigo-500 via-sky-500 to-blue-500">
+                  {title}
+                </span>
+              </h3>
+              <p className="text-sm text-gray-600">
+                {dayjs(date).format('YYYY年MM月DD日')}
+              </p>
             </Link>
           ))}
         </div>
